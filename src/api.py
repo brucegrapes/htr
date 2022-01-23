fb6c7b5c542cdb38c0e2154bf2c44fa8fa8b7d6d
brucegrapes <govardanan.gms@gmail.com>
2022-01-23 12:44:10 +0530

    Refactor api

--- src/api.py
+++ src/api.py
@@ -6,6 +6,7 @@ from createTextFile import createTextFromImages
 from plag import checkPlagFunc
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'A-Z1-9' #<--- SECRET_KEY must be set in config to access session
 
 UPLOAD_FOLDER = '../input-images'
 TEXT_OUTPUT_FOLDER = '../output-files'
@@ -19,18 +20,24 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/',methods=['GET'])
+def welcome():
+    return 'Welcome'
+
+@app.route('/upload_file', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
+            print('No file part')
             return redirect(request.url)
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
+            print('No Selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
